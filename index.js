f59d0e0f171fa47c0f2017e01cbe96c57c79c00f
Kenneth Tschida <158073504+Kenny-Tschida@users.noreply.github.com>
2024-04-04 13:16:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,23 +7,144 @@ import { useForm } from "react-hook-form";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App(){
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [dataF,setDataF] = useState({});
+  const [viewCatalog, setViewCatalog] = useState(true);
+  const [viewer,setViewer] = useState(0);
+
   function Shop() {
-    
-  }
-  function Cart() {
+    function toCart() {
+      setViewCatalog(!viewCatalog);
+    }
 
+    useEffect(() => {
+        total();
+    }, [cart]);
+
+    const total = () => {
+        let totalVal = 0;
+        for (let i = 0; i < cart.length; i++) {
+            totalVal += cart[i].price;
+        }
+        setCartTotal(totalVal);
+    };
+
+    const addToCart = (el) => {
+        setCart([...cart, el]);
+    };
+
+    const removeFromCart = (el) => {
+      let itemFound = false;
+      const updatedCart = cart.filter((cartItem) => {
+          if (cartItem.id === el.id && !itemFound) {
+              itemFound = true;
+              return false;
+          }
+              return true;
+      });
+      if (itemFound) {
+          setCart(updatedCart);
+      }
+    };
+
+    const cartItems = cart.map((el) => (
+        <div key={el.id} class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+                <img class="img-fluid" src={el.image} width={150} />
+            </div>
+            <div class="col">
+                {el.title}
+            </div>
+            <div class="col">
+                ${el.price}
+            </div>
+        </div>
+    ));
+
+    function howManyofThis(id) {
+        let hmot = cart.filter((cartItem) => cartItem.id === id);
+        return hmot.length;
+    }
+
+    const listItems = cart.map((el) => (
+      <div class="col" key={el.id}>
+          <div class="card shadow-sm">
+              <img src={el.image} width="100%" alt="image" />
+              <div class="card-body">
+                  <p class="card-text"><strong>{el.title}</strong></p>
+                  <p class="card-text">{el.description}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                          <button class="mx-1" type="button" variant="light" onClick={() => removeFromCart(el)} > - </button>{" "}
+                          <button class="mx-1" type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+                          ${el.price} <span class="close">&#10005;</span>{howManyofThis(el.id)}
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    ));
+
+    return(
+        <div>
+          <header data-bs-theme="dark">
+            <div id="navbarHeader">
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm-8 col-md-7 py-4">
+                    <h2>Outdoor Emporium: Browse</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+    );
   }
+
   function Payment() {
-
+    return(
+      <div>
+        <header data-bs-theme="dark">
+          <div id="navbarHeader">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                  <h2>Outdoor Emporium: Payment and Cart</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <button></button>
+      </div>
+    );
   }
+
   function Summary() {
-
+    return(
+      <div>
+        <header data-bs-theme="dark">
+          <div id="navbarHeader">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                  <h2>Outdoor Emporium: Summary</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
   }
+
   return(<div>
     {viewer === 0 && <Shop />}
-    {viewer === 1 && <Cart />}
-    {viewer === 2 && <Payment />}
-    {viewer === 3 && <Summary />}
+    {viewer === 1 && <Payment />}
+    {viewer === 2 && <Summary />}
   </div>);
 }
 
